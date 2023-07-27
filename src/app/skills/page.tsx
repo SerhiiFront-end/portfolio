d323490655a733d6d6c2d8f0b834a0fc91f2b7e7
Serhii <aorl9048@gmail.com>
2023-07-27 22:23:53 +0100

    commit

--- src/app/skills/page.tsx
+++ src/app/skills/page.tsx
@@ -1,30 +1,25 @@
-'use client'
-import { useRouter } from 'next/navigation'
 import Template from '../../../components/template/template'
 import skills from './skills.json'
 import styles from './skills.module.scss'
 export default function page() {
-	const router = useRouter()
 	return (
-		<div>
-			<Template loading={false} left={false}>
-				<table className={styles.table}>
-					<thead>
-						<tr>
-							<th>Skill / Knowledge </th>
-							<th>Description</th>
+		<Template loading={false} left={false}>
+			<table className={styles.table}>
+				<thead>
+					<tr>
+						<th>Skill / Knowledge </th>
+						<th>Description</th>
+					</tr>
+				</thead>
+				<tbody>
+					{skills.map(el => (
+						<tr key={el.id}>
+							<td className='align-top'>{el.skill}</td>
+							<td>{el.description}</td>
 						</tr>
-					</thead>
-					<tbody>
-						{skills.map(el => (
-							<tr key={el.id}>
-								<td className='align-top'>{el.skill}</td>
-								<td>{el.description}</td>
-							</tr>
-						))}
-					</tbody>
-				</table>
-			</Template>
-		</div>
+					))}
+				</tbody>
+			</table>
+		</Template>
 	)
 }
