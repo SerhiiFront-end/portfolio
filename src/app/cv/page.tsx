b736884d1f1b2415994c19f014177474d571ef71
Serhii <aorl9048@gmail.com>
2023-08-05 22:51:12 +0100

    CV image

--- src/app/cv/page.tsx
+++ src/app/cv/page.tsx
@@ -1,20 +1,19 @@
-// import Image from 'next/image'
-// import CVimg from './CVimg.jpg'
+import Image from 'next/image'
 import Anim from '../../../components/animscroll/Anim'
-import CV from '../../../components/myCV/CV'
 import Template from '../../../components/template/template'
+import CVimg from './CVimg.png'
 export default function page() {
 	return (
 		<div>
-			<Anim isOneTime={false} tag='div'>
-				<CV />
-			</Anim>
 			<Template loading={false} left={false}>
-				<a href='/CV_Serhii_Kozin_UI_UX_dev.docx' download>
-					<button className='w-1/2 block my-3 m-auto md:w-1/5 p-3 bg-slate-700 hover:ease-linear hover:duration-500 hover:bg-slate-500 hover:rounded-md'>
-						Download CV
-					</button>
-				</a>
+				<Image className='m-auto w-[70%]' src={CVimg} alt='CV' />
+				<Anim isOneTime={false} tag='div'>
+					<a href='/CV_Serhii_Kozin_UI_UX_dev.pdf' download>
+						<button className='w-1/2 block my-3 m-auto md:w-1/5 p-3 bg-slate-700 hover:ease-linear hover:duration-500 hover:bg-slate-500 hover:rounded-md'>
+							Download CV
+						</button>
+					</a>
+				</Anim>
 			</Template>
 		</div>
 	)
