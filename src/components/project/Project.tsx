a14e071d6ec5d13894faa12b6e1e71b18b08b480
Serhii <aorl9048@gmail.com>
2023-08-18 23:51:30 +0100

    projects styles page

--- src/components/project/Project.tsx
+++ src/components/project/Project.tsx
@@ -19,23 +19,27 @@ export default function Project({
 	newPage: boolean
 }) {
 	return (
-		<Link href={link} target={newPage ? '_blank' : ''}>
-			<div
-				className={`m-4 md:m-0 md:flex items-center py-5 px-3 border md:border-2 border-black hover:bg-stone-700 md:mt-[10vh] hover:duration-500 hover:ease-linear ${
-					index % 2 === 0 ? '' : 'flex-row-reverse'
-				} `}
-			>
-				<div>
-					<Image src={img} alt={name} width={1200} height={800} />
-				</div>
-				<Anim className='md:text-sm text-xs flex flex-col md:w-3/5 p-3 m-auto w-full'>
-					<h1 className='font-bold text-base md:text-lg text-center text-cyan-400'>
-						{name}
-					</h1>
-					<p className='mt-5'> {description}</p>
-					<Button className='block m-auto mt-5'>Visit</Button>
-				</Anim>
+		<div
+			className={`md:h-[300px] m-4 md:m-0 md:flex items-center border-t md:border-t-0 py-5 px-3 md:mt-[10vh]  ${
+				index % 2 === 0 ? '' : 'flex-row-reverse'
+			} `}
+		>
+			<div className=''>
+				<Image src={img} alt={name} width={1200} height={800} />
 			</div>
-		</Link>
+			<Anim
+				className={`md:text-sm text-xs flex flex-col md:w-3/5 p-3 bg-gray-900 z-10 ${
+					index % 2 === 0 ? 'pl-6' : 'pr-6'
+				} m-auto w-full`}
+			>
+				<h1 className='font-bold text-base md:text-lg text-center text-cyan-400'>
+					{name}
+				</h1>
+				<p className='mt-5'> {description}</p>
+				<Link href={link} target={newPage ? '_blank' : ''}>
+					<Button className='block m-auto mt-5'>Visit</Button>
+				</Link>
+			</Anim>
+		</div>
 	)
 }
